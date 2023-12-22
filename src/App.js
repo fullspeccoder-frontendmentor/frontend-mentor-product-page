fbcc6848b46015bd34f8834b15295266b06bb6fb
Jacob Wilson <jcbwlsn04@gmail.com>
2023-12-22 13:05:20 -0600

    Added navbar with some basic styles to get started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Image from "./images/avatars/image-avatar.png";
+import { ReactComponent as CartIcon } from "./images/icons/icon-cart.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
     </div>
   );
 }
+
+const Nav = () => {
+  return (
+    <nav className="Nav">
+      <div className="navigation">
+        <h1>sneakers</h1>
+        <ul>
+          <li>Collections</li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className="user-container">
+        <CartIcon className="cart-icon" />
+        <img src={Image} alt="profile" />
+      </div>
+    </nav>
+  );
+};
 
 export default App;
